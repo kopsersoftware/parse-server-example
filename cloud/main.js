80132d81cf32ea61d45f6166c6627fb3a2383ea5
kopsersoftware <Hayden@kopsersoftware.com>
2016-08-27 18:24:33 -0400

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,30 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define('hello', function(req, res) {
+  res.success('Hi');
+});
+
+Parse.Cloud.define('sendPushToCustomers', function(request, response){
+
+	var objectId = request.params.objectID;
+	var message = request.params.message;
+
+	Parse.Push.send({
+  	channels: objectId,
+  	data: {
+    alert: message;
+  }
+}, {
+  success: function() {
+    
+    response.success('SUCCESSFULLY SENT');
+  },
+  error: function(error) {
+
+  	response.error(error);
+  },
+  useMasterKey: true
+});
+});
