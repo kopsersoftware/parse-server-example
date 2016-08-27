3b0743425751ca5967ef6070db36a799eea1d3dd
kopsersoftware <Hayden@kopsersoftware.com>
2016-08-27 18:35:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://heroku_cm184hv4:5f2alq5494sh8rn6m919dk68n6@ds021701.mlab.com:21701/heroku_cm184hv4',
+  //databaseURI: databaseUri || 'mongodb://heroku_cm184hv4:5f2alq5494sh8rn6m919dk68n6@ds021701.mlab.com:21701/heroku_cm184hv4',
+  databaseURI: 'mongodb://heroku_cm184hv4:5f2alq5494sh8rn6m919dk68n6@ds021701.mlab.com:21701/heroku_cm184hv4',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'M0f58usNxNWHrnE0pUhhkFecO6uWEnnApZTXUrmQ',
   masterKey: process.env.MASTER_KEY || 'dMN5E3kDrHyQT1M8k7Rb0iHDEFZuSW5Xj37DZu6t',
-  serverURL: process.env.SERVER_URL || 'http://smallbusinessios.herokuapp.com/parse'
+  //serverURL: process.env.SERVER_URL || 'http://smallbusinessios.herokuapp.com/parse'
+  serverURL: 'http://smallbusinessios.herokuapp.com/parse'
 });
 
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
