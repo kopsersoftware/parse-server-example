5e4f0ab072bc9c1a74e594b0c9187c94952e64dc
kopsersoftware <Hayden@kopsersoftware.com>
2016-08-27 18:07:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,11 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '' //Add your master key here. Keep it secret!
+  appId: "M0f58usNxNWHrnE0pUhhkFecO6uWEnnApZTXUrmQ",
+  masterKey: "dMN5E3kDrHyQT1M8k7Rb0iHDEFZuSW5Xj37DZu6t"
+  serverURL: "http://smallbusinessios.herokuapp.com/parse"
 });
+
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
